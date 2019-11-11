ba2ff28fc96e5be72c031a91725d2d6da7d1bea5
Daniel Lowe <daniel.lowe@ecom-macbook38.mynet>
2019-11-11 22:28:13 +0000

    Remove yarn lock and update location config with correct lat and long

--- src/config/locationConfig.js
+++ src/config/locationConfig.js
@@ -1,28 +1,28 @@
 const locationConfig = [
   {
     name: 'London',
-    latitude: 0,
-    longitude: 0,
+    latitude: 51.5074,
+    longitude: 0.1278,
   },
   {
     name: 'Paris',
-    latitude: 0,
-    longitude: 0,
+    latitude: 48.8566,
+    longitude: 2.3522,
   },
   {
     name: 'New York',
-    latitude: 0,
-    longitude: 0,
+    latitude: 40.7128,
+    longitude: -74.0060,
   },
   {
     name: 'Singapore',
-    latitude: 0,
-    longitude: 0,
+    latitude: 1.3521,
+    longitude: 103.8198,
   },
   {
     name: 'Sydney',
-    latitude: 0,
-    longitude: 0,
+    latitude: -33.8688,
+    longitude: 151.2093,
   },
 ];
 
