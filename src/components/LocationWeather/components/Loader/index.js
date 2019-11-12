b1059e7788db3d5e8d15acea4031b550d75e7e01
Daniel Lowe <daniel.lowe@ecom-macbook38.mynet>
2019-11-12 22:42:09 +0000

    Loader component and simplify Location Weather scss

---
+++ src/components/LocationWeather/components/Loader/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Skeleton from '@material-ui/lab/Skeleton';
+import LocationWeatherClasses from '../../LocationWeather.module.scss';
+import WeatherIconClasses from '../WeatherIcon/WeatherIcon.module.scss';
+
+const Loader = () => (
+  <div data-testid="weather-holder" className={LocationWeatherClasses['weather-holder']}>
+    <Skeleton variant="rect" width={100} />
+    <Skeleton className={WeatherIconClasses.icon} variant="rect" width={100} height={100} />
+    <Skeleton variant="rect" width={100} />
+  </div>
+
+);
+
+export default Loader;
--- src/components/LocationWeather/index.js
+++ src/components/LocationWeather/index.js
@@ -3,18 +3,21 @@ import PropTypes from 'prop-types';
 import api from './api';
 import Classes from './LocationWeather.module.scss';
 import WeatherIcon from './components/WeatherIcon';
+import Loader from './components/Loader';
 
 const LocationWeather = ({ activeLocation }) => {
   const [weatherData, setWeatherData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const getLocationWeather = useCallback(async () => {
     try {
+      setLoading(true);
       const { data } = await api.getWeatherData(activeLocation);
       setWeatherData(data);
     } catch {
       //Handle Error
     } finally {
-      //Add loading
+      setLoading(false)
     }
   }, [activeLocation]);
 
@@ -26,12 +29,16 @@ const LocationWeather = ({ activeLocation }) => {
     <div className={Classes.location}>
       <h1 data-testid="location-header">{activeLocation}</h1>
 
-      {weatherData && (
+      {weatherData && !loading && (
         <div data-testid="weather-holder" className={Classes['weather-holder']}>
           <p data-testid="weather-summary" className={Classes.summary}>{weatherData.currently.summary}</p>
           <WeatherIcon icon={weatherData.currently.icon} />
           <p data-testid="weather-temperature" className={Classes.temperature}>{Math.round(weatherData.currently.temperature)}&deg;c</p>
         </div>
+      )}
+
+      {loading && (
+        <Loader />
       )}
     </div>
   );
